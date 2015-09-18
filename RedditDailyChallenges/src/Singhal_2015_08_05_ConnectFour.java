613cdbcbf45049c20042617d08ebb286d2299a98
singhalmanu9 <singhalmanu9@gmail.com>
2015-09-18 19:48:07 -0400

    added a sample file to show how naming should be done

---
+++ RedditDailyChallenges/src/Singhal_2015_08_05_ConnectFour.java
@@ -0,0 +1,17 @@
+
+/**
+ * This class is the Connect Four Reddit Daily Challenge, posted
+ * on 8 August 2015. The full details can be found at 
+ * https://www.reddit.com/r/dailyprogrammer/comments/3fva66/20150805_challenge_226_intermediate_connect_four/.
+ * 
+ * @author Manu S.
+ *
+ */
+public class Singhal_2015_08_05_ConnectFour {
+
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
